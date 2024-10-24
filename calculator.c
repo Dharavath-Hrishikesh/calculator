013cb71af3049afbea4cb93c96bdf50c9976e4fe
Dharavath Hrishikesh <24je0614@iitism.ac.in>
2024-10-24 11:10:04 +0530

    inverse implemented

--- calculator.c
+++ calculator.c
@@ -25,6 +25,13 @@ int square(int i){
 int cube(int i){
 	return i*i*i;
 }
+float inverse(int i){
+	if(i==0){
+		printf("Error:inverse of zero is not defined. \n");
+		return 0;
+	}
+	return 1.0/i;
+}
 int main() {
    
     int a, b;
@@ -80,7 +87,8 @@ int main() {
             case 'i':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+               if(a!=0){
+	       	printf("the inverse of %d is %.2f\n",a,inverse(a));}
                 break;
             default:
                 printf("Invalid operation. Please try again.\n");
